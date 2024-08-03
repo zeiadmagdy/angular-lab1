da1aeb153286e7b6e9635b5d6e69209d5a4dd395
Zeiad Magdy <zeiadmagdy2019@gmail.com>
2024-08-03 04:01:20 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeroSectionComponent,AboutComponent,SkillsComponent,FooterComponent,PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
